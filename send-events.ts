92b0703a442a36a3b04e2e64d5f720e0703166c1
Manish Honnatti <mhonnatti@Manishs-MacBook-Air.local>
2023-01-08 00:53:09 -0800

    additions to test events

--- send-events.ts
+++ send-events.ts
@@ -10,12 +10,39 @@ async function main() {
         }
 
         try {
-            await axios.post(`http://localhost:3000/${endpoint}`, message)
+            await axios.post(`http://localhost:3000/${endpoint}`, message).then(res => {
+                console.log(res.data);
+            })
         } catch (error) {
             console.error(error.code)
         }
 
     }
+
+    try {
+        await axios.get(`http://localhost:3000/organizations/381f5cc5-dfe4-4f58-98ad-116666855ca3`).then(res => {
+            console.log(res.data);
+        })
+    } catch (error) {
+        console.error(error.code)
+    }
+
+    try {
+        await axios.get(`http://localhost:3000/shipments/S00001175`).then(res => {
+            console.log(res.data);
+        })
+    } catch (error) {
+        console.error(error.code)
+    }
+
+    try {
+      await axios.get(`http://localhost:3000/weights/total/tonne`).then(res => {
+          console.log(res.data);
+      })
+  } catch (error) {
+      console.error(error.code)
+  }
+
 }
 
 main()
